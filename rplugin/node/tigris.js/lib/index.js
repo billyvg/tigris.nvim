10a9eb3c346d7565e2bf2bdaed652eddfe287354
Billy Vong <billy@opstarts.com>
2016-08-28 07:31:15 -0700

    Update build

--- rplugin/node/tigris.js/lib/index.js
+++ rplugin/node/tigris.js/lib/index.js
@@ -1,64 +1,46 @@
-/**
- * Javascript syntax parsing with babylon
- *
- * @author Billy Vong <github at mmo.me>
- * @license MIT
- */
-'use strict' // eslint-disable-line
-;
+'use strict';
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { var callNext = step.bind(null, "next"); var callThrow = step.bind(null, "throw"); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(callNext, callThrow); } } callNext(); }); }; }
+var _vimSyntaxParser = require('vim-syntax-parser');
 
-var parser = require('vim-syntax-parser').default;
-var _ = require('lodash');
-var regeneratorRuntime = require('regenerator-runtime/runtime'); // eslint-disable-line
+var _vimSyntaxParser2 = _interopRequireDefault(_vimSyntaxParser);
 
-// vim vars
-var ENABLE_VAR = 'tigris#enabled';
-// const AUTO_START_VAR = 'tigris#auto_enable';
-var DEBUG_VAR = 'tigris#debug';
-var DELAY_VAR = 'tigris#delay'; // eslint-disable-line
-var FLY_VAR = 'tigris#on_the_fly_enabled';
-var EXT_VAR = 'tigris#extensions'; // eslint-disable-line
+var _lodash = require('lodash');
 
-// defaults
-var DELAY_DEFAULT = 500;
-var EXT_DEFAULT = ['*.js', '*.jsx']; // eslint-disable-line
-var ERR_ID = 12345;
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _runtime = require('regenerator-runtime/runtime');
+
+var _runtime2 = _interopRequireDefault(_runtime);
+
+var _promisifyNode = require('promisify-node');
+
+var _promisifyNode2 = _interopRequireDefault(_promisifyNode);
+
+var _checkForUpdate = require('./checkForUpdate');
+
+var _checkForUpdate2 = _interopRequireDefault(_checkForUpdate);
+
+var _constants = require('./constants');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { var callNext = step.bind(null, "next"); var callThrow = step.bind(null, "throw"); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(callNext, callThrow); } } callNext(); }); }; } /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * Javascript syntax parsing with babylon
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                *
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * @author Billy Vong <github at mmo.me>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * @license MIT
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                */
+// eslint-disable-line
+
+// Check for updates
 
 var DEBUG_MAP = new Map();
 var HL_MAP = new Map();
 
-// Check for updates
-var updateNotifier = require('update-notifier');
-var pkg = require('../package.json');
-var promisify = require('promisify-node');
-
-function checkForUpdates(nvim) {
-  var notifier = updateNotifier({
-    pkg: pkg,
-    updateCheckInterval: 1000 * 60 * 60 * 6
-  });
-
-  if (notifier && notifier.update) {
-    if (nvim) {
-      var updateMsg = '[tigris] Update available ' + notifier.update.current + ' →\n        ' + notifier.update.latest;
-
-      debug(updateMsg);
-      nvim.command('echomsg \'' + updateMsg + '\'');
-      nvim.command('\n        echo \'[tigris]\' |\n        echon \' Update available \' |\n        echohl Comment |\n        echon \'' + notifier.update.current + '\' |\n        echohl None |\n        echon \' → \' |\n        echohl Keyword |\n        echon \'' + notifier.update.latest + '\' |\n        echohl None\n      ');
-    }
-
-    return notifier.update;
-  }
-
-  return null;
-}
-
 var highlight = function highlight(buffer, id, name, lineStart, columnStart, columnEnd, isDebug) {
   // Save highlighting group for debugging
   if (isDebug) {
-    _.range(columnEnd - columnStart + 1).forEach(function (num) {
+    _lodash2.default.range(columnEnd - columnStart + 1).forEach(function (num) {
       var key = lineStart + 1 + ',' + (columnStart + num); // [lineStart, columnStart + num];
       if (!DEBUG_MAP.has(key)) {
         DEBUG_MAP.set(key, []);
@@ -80,7 +62,7 @@ var highlight = function highlight(buffer, id, name, lineStart, columnStart, col
 };
 
 var parse = (function () {
-  var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+  var _ref = _asyncToGenerator(_runtime2.default.mark(function _callee2() {
     var _this = this;
 
     var _ref2 = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
@@ -89,50 +71,58 @@ var parse = (function () {
     var filename = _ref2.filename;
     var clear = _ref2.clear;
 
-    var api, prStart, _file, start, enabled;
+    var api, _start, key, _key, _file, start, enabled;
 
-    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+    return _runtime2.default.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
             api = nvim;
-            prStart = +new Date();
 
-            Object.keys(nvim).forEach(function (key) {
-              if (typeof nvim[key] === 'function') {
-                api[key] = promisify(nvim[key]);
+            try {
+              _start = +new Date();
+
+              for (key in nvim) {
+                // eslint-disable-line
+                if (typeof nvim[key] === 'function') {
+                  api[key] = (0, _promisifyNode2.default)(nvim[key]);
+                }
               }
-            });
-            debug('Promisifiy overhead ' + (new Date() - prStart));
-
-            if (!filename) {
-              debug('ERROR NO FILENAME');
+              for (_key in nvim.Buffer) {
+                // eslint-disable-line
+                if (typeof nvim.Buffer[_key] === 'function') {
+                  api.Buffer[_key] = (0, _promisifyNode2.default)(nvim.Buffer[_key]);
+                }
+              }
+              debug('Promisifiy overhead ' + (new Date() - _start) + 'ms');
+            } catch (err) {
+              debug('Error promisifying nvim api', err, err.stack);
             }
 
-            _file = filename.split('/').pop();
+            _file = filename && filename.split('/').pop() || '';
 
             debug('[' + _file + '] Parse called');
             start = +new Date();
-            _context2.prev = 8;
-            _context2.next = 11;
-            return api.getVar(ENABLE_VAR);
+            _context2.prev = 5;
+            _context2.next = 8;
+            return api.getVar(_constants.ENABLE_VAR);
 
-          case 11:
+          case 8:
             enabled = _context2.sent;
 
             if (!enabled) {
-              _context2.next = 14;
+              _context2.next = 11;
               break;
             }
 
-            return _context2.delegateYield(regeneratorRuntime.mark(function _callee() {
+            return _context2.delegateYield(_runtime2.default.mark(function _callee() {
               var isDebug, buffer, lines, newId, results, highlightPromises;
-              return regeneratorRuntime.wrap(function _callee$(_context) {
+              return _runtime2.default.wrap(function _callee$(_context) {
                 while (1) {
                   switch (_context.prev = _context.next) {
                     case 0:
                       _context.next = 2;
-                      return api.getVar(DEBUG_VAR);
+                      return api.getVar(_constants.DEBUG_VAR);
 
                     case 2:
                       isDebug = _context.sent;
@@ -160,7 +150,7 @@ var parse = (function () {
                       debug('[' + _file + '/' + newId + '] Calling tigris parser');
                       _context.prev = 15;
                       _context.next = 18;
-                      return parser(lines.join('\n'), {
+                      return (0, _vimSyntaxParser2.default)(lines.join('\n'), {
                         plugins: ['asyncFunctions', 'asyncGenerators', 'classConstructorCall', 'classProperties', 'decorators', 'doExpressions', 'exponentiationOperator', 'exportExtensions', 'flow', 'functionSent', 'functionBind', 'jsx', 'objectRestSpread', 'trailingFunctionCommas']
                       });
 
@@ -180,15 +170,15 @@ var parse = (function () {
                       // should highlight errors?
                       if (_context.t0 && _context.t0.loc) {
                         // Clear previous error highlight
-                        buffer.clearHighlight(ERR_ID, 0, -1);
-                        buffer.addHighlight(ERR_ID, 'Error', _context.t0.loc.line - 1, 0, -1);
+                        buffer.clearHighlight(_constants.ERR_ID, 0, -1);
+                        buffer.addHighlight(_constants.ERR_ID, 'Error', _context.t0.loc.line - 1, 0, -1);
                       }
 
                     case 26:
 
                       if (results && results.length) {
                         // Clear error highlight
-                        buffer.clearHighlight(ERR_ID, 0, -1);
+                        buffer.clearHighlight(_constants.ERR_ID, 0, -1);
 
                         highlightPromises = results.map(function (result) {
                           // wtb es6
@@ -205,7 +195,7 @@ var parse = (function () {
                           debug('Parse time: ' + (end - start) + 'ms');
 
                           if (clear) {
-                            _.range(1, newId - 2).forEach(function (num) {
+                            _lodash2.default.range(1, newId - 2).forEach(function (num) {
                               buffer.clearHighlight(num, 0, -1);
                             });
                           }
@@ -213,7 +203,7 @@ var parse = (function () {
                           if (filename) {
                             var oldId = HL_MAP.get(filename);
                             if (oldId) {
-                              debug('[' + _file + '::' + oldId + '] Clearing old highlight');
+                              // debug(`[${_file}::${oldId}] Clearing old highlight`);
                               buffer.clearHighlight(oldId, 0, -1);
                             }
 
@@ -230,24 +220,24 @@ var parse = (function () {
                   }
                 }
               }, _callee, _this, [[15, 21]]);
-            })(), 't0', 14);
+            })(), 't0', 11);
 
-          case 14:
-            _context2.next = 19;
+          case 11:
+            _context2.next = 16;
             break;
 
-          case 16:
-            _context2.prev = 16;
-            _context2.t1 = _context2['catch'](8);
+          case 13:
+            _context2.prev = 13;
+            _context2.t1 = _context2['catch'](5);
 
             debug('Error', _context2.t1);
 
-          case 19:
+          case 16:
           case 'end':
             return _context2.stop();
         }
       }
-    }, _callee2, this, [[8, 16]]);
+    }, _callee2, this, [[5, 13]]);
   }));
 
   return function parse(_x) {
@@ -256,7 +246,7 @@ var parse = (function () {
 })();
 
 function handleBufEnter(nvim, filename) {
-  checkForUpdates(nvim);
+  (0, _checkForUpdate2.default)(nvim);
 
   debug('[' + filename.split('/').pop() + '] Handle buffer enter');
 
@@ -267,14 +257,14 @@ function handleParse(nvim, filename) {
   parse({ nvim: nvim, filename: filename });
 }
 
-var flyParse = _.debounce(function (nvim, filename) {
+var flyParse = _lodash2.default.debounce(function (nvim, filename) {
   debug('[' + filename.split('/').pop() + '] Fly parse called');
-  nvim.getVar(FLY_VAR, function (err, enableFly) {
+  nvim.getVar(_constants.FLY_VAR, function (err, enableFly) {
     if (enableFly) {
       parse({ nvim: nvim, filename: filename });
     }
   });
-}, DELAY_DEFAULT);
+}, _constants.DELAY_DEFAULT);
 
 var clear = function clear(nvim) {
   nvim.getCurrentBuffer(function (err, buffer) {
@@ -285,7 +275,7 @@ var clear = function clear(nvim) {
 };
 
 var enable = function enable(nvim) {
-  nvim.setVar(ENABLE_VAR, true, function (err) {
+  nvim.setVar(_constants.ENABLE_VAR, true, function (err) {
     if (!err) {
       parse({ nvim: nvim });
     }
@@ -293,7 +283,7 @@ var enable = function enable(nvim) {
 };
 
 var disable = function disable(nvim) {
-  nvim.setVar(ENABLE_VAR, false);
+  nvim.setVar(_constants.ENABLE_VAR, false);
 
   clear(nvim);
 };
@@ -307,7 +297,7 @@ plugin.function('_tigris_disable', function (nvim) {
 });
 
 plugin.function('_tigris_toggle', function (nvim) {
-  nvim.getVar(ENABLE_VAR, function (err, enabled) {
+  nvim.getVar(_constants.ENABLE_VAR, function (err, enabled) {
     if (enabled) {
       disable(nvim);
     } else {
@@ -339,7 +329,7 @@ plugin.function('_tigris_parse', function (nvim, args) {
 });
 
 plugin.function('_tigris_highlight_debug', function (nvim) {
-  nvim.getVar(DEBUG_VAR, function (err, isDebug) {
+  nvim.getVar(_constants.DEBUG_VAR, function (err, isDebug) {
     if (isDebug) {
       nvim.getCurrentWindow(function (err, win) {
         win.getCursor(function (err, pos) {
